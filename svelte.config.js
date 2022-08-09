13eb22c0d65e23b2dd1ce9564fbb03c08132bb1d
Cesar Velasco <cesar.velasco@akimad.com>
2022-08-09 18:45:55 -0500

    :rocket: change in svelte config paths

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
+
 import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
+
+const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +11,10 @@ const config = {
 			pages:"docs",
 			assets:"docs",
 			fallback: '200.html'
-		})
+		}),
+		paths:{
+			base: dev ? "" : "/carvesco"
+		}
 	},
 	preprocess:[
 		preprocess({
