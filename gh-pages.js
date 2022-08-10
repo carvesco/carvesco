ed421cbd4dd65208a736d63889ed6cf8915276c3
Cesar Velasco <cesar.velasco@akimad.com>
2022-08-09 19:17:33 -0500

    :deploy: fix gh-pages deploy file

--- gh-pages.js
+++ gh-pages.js
@@ -2,10 +2,10 @@
 import ghpages from 'gh-pages'
 
 ghpages.publish(
-  'public', // path to public directory
+  'build', // path to public directory
   {
     branch: 'gh-pages',
-    repo: 'https://github.com/carvesco/carvesco.git', // Update to point to your repository
+    repo: 'https://github.com/carvesco/carvesco-page.git', // Update to point to your repository
     user: {
       name: 'carvesco', // update to use your name
       email: 'cavelascor@unal.edu.co' // Update to use your email
