6ffe7f174004ac8fd77d66593c066b88d87c466d
Cesar Velasco <cesar.velasco@akimad.com>
2022-08-09 19:39:27 -0500

    :rocket: fix deployment vercel

--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/__layout.svelte.js';
-
-export { module };
-export const index = 0;
-export const file = '_app/immutable/pages/__layout.svelte-aa171b28.js';
-export const imports = ["_app/immutable/pages/__layout.svelte-aa171b28.js","_app/immutable/chunks/index-e7d4872c.js"];
-export const stylesheets = ["_app/immutable/assets/__layout-5aa03fd5.css"];
--- .vercel/output/functions/render.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const index = 2;
-export const file = '_app/immutable/pages/index.svelte-47bac1d3.js';
-export const imports = ["_app/immutable/pages/index.svelte-47bac1d3.js","_app/immutable/chunks/index-e7d4872c.js"];
-export const stylesheets = [];
