487bc003ac736826252dfe4ea69df5d27dc6747b
Cesar Velasco <cesar.velasco@akimad.com>
2022-08-24 16:27:57 -0500

    :sparkles: added more subitme to programmin section

--- src/components/programming/programmingItem/programmingItem.svelte
+++ src/components/programming/programmingItem/programmingItem.svelte
@@ -2,8 +2,11 @@
 	import ProgrammingSubItem from './programmingSubItem/programmingSubItem.svelte';
 </script>
 
-<div class="w-4/5 m-auto mt-7 grid grid-cols-4">
+<div class="w-4/5 m-auto mt-7 grid grid-cols-4 gap-4">
 	<ProgrammingSubItem pName={'JavaScript'} pImage={'images/JavaScriptLogo.png'} />
     <ProgrammingSubItem pName={'React'} pImage={'images/ReactLogo.png'} />
     <ProgrammingSubItem pName={'NodeJS'} pImage={'images/NodeJSLogo.png'} />
+    <ProgrammingSubItem pName={'Go'} pImage={'images/GoLogo.png'} />
+    <ProgrammingSubItem pName={'MongoDB'} pImage={'images/MongoDBLogo.png'} />
+    <ProgrammingSubItem pName={'Git'} pImage={'images/GitLogo.png'} />
 </div>
